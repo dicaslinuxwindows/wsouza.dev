e7c588ee0537bb0d2801f46cda260c46ea6a30bf
Wagner A. de Souza <dicaslinuxewindows@gmail.com>
2021-12-12 15:31:12 -0300

    Adicionando arquivos dom 12 dez 2021 15:31:12 -03

---
+++ publica.sh
@@ -0,0 +1,40 @@
+!/bin/sh
+
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# Create commit message
+msg="Adicionando arquivos $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+
+# Build the project.
+echo ""
+echo ""
+echo "Committing changes to $(pwd)"
+hugo -D
+
+# Go To Public folder
+#cd public
+
+# Add 'public' (Github Pages repo) changes to git and commit/push.
+#echo ""
+#echo ""
+#echo "Committing changes to $(pwd)"
+#git add .
+#git commit -m "$msg"
+#git push 
+
+# Add this repos changes to git and commit/push. First 'cd' out of public
+#cd
+#cd Git/wsouza.dev
+
+echo ""
+echo ""
+echo "Committing changes to $(pwd)"
+git add .
+git commit -m "$msg"
+git push 
